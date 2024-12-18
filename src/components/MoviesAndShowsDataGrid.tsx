be83190bb2f2ea81fdb1fa14e418daf4e118ae1a
Alex Brodnicki <apbrodnicki@gmail.com>
2024-12-18 10:29:44 -0600

    Move data grid columns into separate file and update overall styling of table and manipulate data better for display.

--- src/components/MoviesAndShowsDataGrid.tsx
+++ src/components/MoviesAndShowsDataGrid.tsx
@@ -1,5 +1,5 @@
-import { Link, Typography } from '@mui/material';
-import { DataGrid, type GridColDef } from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
+import { getDataGridColumns } from 'helper/getDataGridColumns';
 import type { CsvRecord } from 'models';
 import React from 'react';
 
@@ -7,69 +7,26 @@ interface MoviesAndShowsDataGridProps {
 	records: CsvRecord[];
 }
 
-export const MoviesAndShowsDataGrid = ({ records }: MoviesAndShowsDataGridProps): React.JSX.Element => {
-	const columns: GridColDef[] = [
-		{
-			field: 'title',
-			headerName: 'Title',
-			renderCell: (param) => (
-				<Link href={param.row.imdbUrl} target='_blank'>
-					<Typography>{param.value}</Typography>
-				</Link>
-			)
-		},
-		{
-			field: 'userRating',
-			headerName: 'User Rating',
-			renderCell: (param) => <Typography>{param.value}</Typography>
-		},
-		{
-			field: 'imdbRating',
-			headerName: 'IMDB Rating',
-			renderCell: (param) => <Typography>{param.value}</Typography>
-		},
-		{
-			field: 'votes',
-			headerName: 'Votes',
-			renderCell: (param) => <Typography>{param.value}</Typography>
-		},
-		{
-			field: 'releaseDate',
-			headerName: 'Release Date',
-			renderCell: (param) => <Typography>{param.value.getFullYear()}</Typography>
-		},
-		{
-			field: 'type',
-			headerName: 'Type',
-			renderCell: (param) => <Typography>{param.value}</Typography>
-		},
-		{
-			field: 'directors',
-			headerName: 'Director(s)',
-			renderCell: (param) => <Typography>{param.value}</Typography>
-		},
-		{
-			field: 'genres',
-			headerName: 'Genre(s)',
-			renderCell: (param) => <Typography>{param.value}</Typography>
-		},
-		{
-			field: 'runtime',
-			headerName: 'Runtime',
-			renderCell: (param) => <Typography>{param.value}</Typography>
-		},
-		{
-			field: 'description',
-			headerName: 'Description',
-			renderCell: (param) => <Typography>{param.value}</Typography>
-		}
-	];
-
-	return (
-		<DataGrid
-			columns={columns}
-			rows={records}
-			getRowId={(row: CsvRecord) => `${row.title} ${row.imdbUrl}`}
-		/>
-	);
-};
+export const MoviesAndShowsDataGrid = ({ records }: MoviesAndShowsDataGridProps): React.JSX.Element => (
+	<DataGrid
+		columns={getDataGridColumns()}
+		rows={records}
+		getRowId={(row: CsvRecord) => `${row.title} ${row.imdbUrl}`}
+		getRowHeight={() => 'auto'}
+		getRowClassName={(params) => params.indexRelativeToCurrentPage % 2 === 0 ? 'even-row' : 'odd-row'}
+		showCellVerticalBorder
+		sx={{
+			height: '600px',
+			'& .MuiDataGrid-cell': {
+				display: 'flex',
+				alignItems: 'center'
+			},
+			// '& .even-row': {
+			// 	backgroundColor: 'gray'
+			// },
+			// '& .odd-row': {
+			// 	backgroundColor: 'light gray'
+			// }
+		}}
+	/>
+);
---
+++ src/helper/getDataGridColumns.tsx
@@ -0,0 +1,111 @@
+import { Link, Typography } from '@mui/material';
+import type { GridColDef } from '@mui/x-data-grid';
+
+export const getDataGridColumns = (): GridColDef[] => ([
+	{
+		field: 'title',
+		headerName: 'Title',
+		align: 'center',
+		headerAlign: 'center',
+		flex: 1,
+		renderCell: (param) => (
+			<Link href={param.row.imdbUrl} target='_blank'>
+				<Typography>{param.value}</Typography>
+			</Link>
+		)
+	},
+	{
+		field: 'userRating',
+		headerName: 'User Rating (/10)',
+		align: 'center',
+		headerAlign: 'center',
+		flex: 1,
+		type: 'number',
+		renderCell: (param) => <Typography>{param.value}</Typography>
+	},
+	{
+		field: 'imdbRating',
+		headerName: 'IMDB Rating (/10)',
+		align: 'center',
+		headerAlign: 'center',
+		flex: 1,
+		type: 'number',
+		renderCell: (param) => <Typography>{param.value}</Typography>
+	},
+	{
+		field: 'votes',
+		headerName: 'Votes',
+		align: 'center',
+		headerAlign: 'center',
+		flex: 1,
+		type: 'number',
+		renderCell: (param) => <Typography>{param.value.toLocaleString()}</Typography>
+	},
+	{
+		field: 'releaseDate',
+		headerName: 'Release Date',
+		align: 'center',
+		headerAlign: 'center',
+		flex: 1,
+		type: 'date',
+		renderCell: (param) => <Typography>{param.value.getFullYear()}</Typography>
+	},
+	{
+		field: 'type',
+		headerName: 'Type',
+		align: 'center',
+		headerAlign: 'center',
+		flex: 1,
+		renderCell: (param) => <Typography>{param.value}</Typography>
+	},
+	{
+		field: 'directors',
+		headerName: 'Director(s)',
+		align: 'center',
+		headerAlign: 'center',
+		flex: 1,
+		renderCell: (param) => <Typography>{param.value}</Typography>
+	},
+	{
+		field: 'genres',
+		headerName: 'Genre(s)',
+		align: 'center',
+		headerAlign: 'center',
+		flex: 1,
+		renderCell: (param) => <Typography>{param.value}</Typography>
+	},
+	{
+		field: 'runtime',
+		headerName: 'Runtime',
+		align: 'center',
+		headerAlign: 'center',
+		flex: 1,
+		type: 'number',
+		renderCell: (param) => {
+			let runtime = '';
+
+			if (param.value === 0) {
+				runtime = 'Unknown';
+			}
+
+			if (param.value > 0 && param.value < 60) {
+				runtime = `${param.value} minutes`;
+			}
+
+			if (param.value === 60) {
+				runtime = '1 hour';
+			}
+
+			if (param.value > 60) {
+				const hours = Math.floor(param.value / 60);
+				const minutes = param.value % 60;
+				runtime = `${hours} hour${hours > 1 ? 's' : ''} and ${minutes} minute${minutes > 1 ? 's' : ''}`;
+			}
+
+
+			return (
+				<Typography>{runtime}</Typography>
+			);
+		}
+	}
+]);
